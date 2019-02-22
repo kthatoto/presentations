11f7086c146c34653a14f8b8d5d9289872c37e83
kthatoto <kthatoto@gmail.com>
2019-02-22 13:24:32 +0900

    Added global style

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,18 @@
 </template>
 
 <style lang="scss">
+body {
+  width: 100%;
+  margin: 0 0;
+}
+html {
+  height: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 100%;
+}
 </style>
