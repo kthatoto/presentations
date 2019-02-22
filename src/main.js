40ef275aa4f90eb42b053aa5960fc01419179962
kthatoto <kthatoto@gmail.com>
2019-02-22 13:21:10 +0900

    First Presentation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Eagle from 'eagle.js'
+import 'eagle.js/dist/eagle.css'
+import 'animate.css'
+Vue.component(Eagle.Slide.name, Eagle.Slide)
+Vue.component(Eagle.Transition.name, Eagle.Transition)
+Vue.use(Eagle)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/presentations/HackingHackAndSlashOnTerminalAktsk.vue
+++ src/views/presentations/HackingHackAndSlashOnTerminalAktsk.vue
@@ -1,7 +1,21 @@
 <template lang="pug">
+slide(:steps=4)
+  eg-transition(enter='slideInRight' leave='slideOutLeft')
+    p(v-if="step >= 1")
+      | {{ step }}
+    p(v-if="step >= 2")
+      | {{ step }}
+    p(v-if="step >= 3")
+      | {{ step }}
+    p(v-if="step >= 4")
+      | {{ step }}
+slide
+  p Perfect
 </template>
 
 <script>
+import { Slideshow } from 'eagle.js'
 export default {
+  mixins: [Slideshow]
 }
 </script>
